458bc9ee7d381c7b87e835b953da560c63dd87f4
Harmz7 <dazzaldhillon97@hotmail.com>
2026-07-24 16:03:10 -0400

    Fix math logic in app.py to pass PyTest

--- cicd-lab8-GitHubActions/app.py
+++ cicd-lab8-GitHubActions/app.py
@@ -1,6 +1,6 @@
 def add_numbers(a, b):
     """Simple function to test in CI/CD pipeline."""
-    return a + b + 10 #intentional bug to demonstrate CI/CD testing failure
+    return a + b
 
 if __name__ == "__main__":
     print(f"2 + 3 = {add_numbers(2, 3)}")
